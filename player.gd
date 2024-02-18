06fe5307f2b4009862937990ec666edd9371fd7a
Gabe Schrock <gabeschrock@gmail.com>
2024-02-18 12:27:25 -0500

    changed level to an enclosed space

--- player.gd
+++ player.gd
@@ -7,10 +7,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
+	if Input.is_action_just_pressed("jump"):
+		linear_velocity.y -= 800
 	if Input.is_action_pressed("move_right"):
 		linear_velocity.x += 400 * delta
 	if Input.is_action_pressed("move_left"):
 		linear_velocity.x -= 400 * delta
-	if Input.is_action_pressed("jump"):
-		linear_velocity.y -= 400
