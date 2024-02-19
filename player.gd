41a3bf670a4784bce02c78f6fb4cabeda5fb682a
Gabe Schrock <gabeschrock@gmail.com>
2024-02-19 10:13:42 -0500

    Fixed 2 of the 3 bugs I know of: Jumping in midair, and not being affected by gravity when pressing left or right.

--- player.gd
+++ player.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+var yPos = position.y
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -7,9 +9,21 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("jump"):
-		linear_velocity.y -= 800
+	pass
+	#if Input.is_action_just_pressed("jump"):
+		#linear_velocity.y -= 800
+	#if Input.is_action_pressed("move_right"):
+		#linear_velocity.x += 400 * delta
+	#if Input.is_action_pressed("move_left"):
+		#linear_velocity.x -= 400 * delta
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("jump") and position.y == yPos:
+		linear_velocity.y = -800
+	yPos = position.y
+	
 	if Input.is_action_pressed("move_right"):
 		linear_velocity.x += 400 * delta
 	if Input.is_action_pressed("move_left"):
 		linear_velocity.x -= 400 * delta
+	
