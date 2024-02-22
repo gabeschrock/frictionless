9f7a8e911b8d6f1a152f4edb2dc4ec49f82567c2
Gabe Schrock <gabeschrock@gmail.com>
2024-02-22 16:27:08 -0500

    added main menu (no level select yet) and updated the tileset

--- player.gd
+++ player.gd
@@ -8,14 +8,8 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
-	#if Input.is_action_just_pressed("jump"):
-		#linear_velocity.y -= 800
-	#if Input.is_action_pressed("move_right"):
-		#linear_velocity.x += 400 * delta
-	#if Input.is_action_pressed("move_left"):
-		#linear_velocity.x -= 400 * delta
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and position.y == yPos:
