ac28cba65dfb4ee407bd8f8f646c50f0c4db3996
Gabe Schrock <gabeschrock@gmail.com>
2024-03-01 08:33:04 -0500

    changed the player to a CharacterBody2D and got spike collision working

--- player.gd
+++ player.gd
@@ -1,30 +1,28 @@
-extends RigidBody2D
-
-var yPos = position.y
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
+extends CharacterBody2D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("jump") and position.y == yPos:
-		linear_velocity.y = -800
-	yPos = position.y
-	
-	if Input.is_action_pressed("move_right"):
-		linear_velocity.x += 400 * delta
-	if Input.is_action_pressed("move_left"):
-		linear_velocity.x -= 400 * delta
-	
-	#var tile_index = danger_tilemap.local_to_map(position)
-	#if danger_tilemap.get_cell_tile_data(0, tile_index):
-		#print("hit danger")
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
-func _on_danger_hitbox_body_entered(_body):
-	print("hit")
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x += direction * SPEED * delta
+	else:
+		velocity.x += move_toward(velocity.x, 0, SPEED) * delta
+
+	move_and_slide()
