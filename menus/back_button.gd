5f75b2e92c8ffe05567f747a4bf8eb59f87eb3cb
gabeschrock <gabeschrock@gmail.com>
2024-04-14 12:39:25 -0400

    fixed bug mentioned previously

--- menus/back_button.gd
+++ menus/back_button.gd
@@ -2,13 +2,18 @@ extends TextureButton
 
 @export var exit_to: String
 
-@onready var main_node = get_tree().root
+@onready var main_node = $/root/Main
 
 func _ready():
-	print(self.filename)
+	pass
+	#while main_node.name != "Main":
+	#	main_node = main_node.get_parent()
+	
+	#main_node = $/root/Main.get_child(0).get_parent()
 
 func _on_pressed():
 	var instance = main_node[exit_to].instantiate()
+	var scene = main_node.get_child(0)
 	
 	main_node.add_child(instance)
-	owner.queue_free()
+	scene.queue_free()
