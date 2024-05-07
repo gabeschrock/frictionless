781567257a838947287a4071835be5a6dd8f1144
gabeschrock <gabeschrock@gmail.com>
2024-05-07 17:18:07 -0400

    added new levels, tweaked level select

--- menus/level_select.gd
+++ menus/level_select.gd
@@ -8,15 +8,3 @@ func open_level(level: int):
 
 func _on_level_1_pressed():
 	open_level(1)
-
-func _on_level_2_pressed():
-	open_level(2)
-
-func _on_level_3_pressed():
-	open_level(3)
-
-func _on_level_4_pressed():
-	open_level(4)
-
-func _on_level_5_pressed():
-	open_level(5)
